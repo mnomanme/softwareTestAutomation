b0d87010253391a31a527f92178e994a2a7e02ce
nomanme <abdullah.noman083@gmail.com>
2022-06-20 12:14:08 +0600

    newman done :construction:

---
+++ 01-Postaman-Test-One/report.js
@@ -0,0 +1,21 @@
+const newman = require('newman');
+
+newman.run(
+	{
+		collection: require('./collection/CaseOneTest.postman_collection.json'), // pointing to local JSON file.
+		// environment: require('./collection/customer_api_env.json'),
+		iterationCount: 1,
+		reporters: 'htmlextra',
+		reporter: {
+			htmlextra: {
+				export: './Reports/report.html',
+			},
+		},
+	},
+	function (err) {
+		if (err) {
+			throw err;
+		}
+		console.log('collection run complete!');
+	}
+);
